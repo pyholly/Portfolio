221fac0462b08d789f401020dbb3222bbae8ccf0
pyholly <pyhshum@gmail.com>
2023-08-21 16:59:51 +0100

    implemented basic dynamic projects list

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,10 +1,19 @@
 import "../src/App.css";
+import { projectList } from "../assets/projectsList";
 
 export const Projects = () => {
   return (
     <section>
       <h2>My Projects</h2>
-      <article></article>
+      {projectList.map((project) => {
+        return (
+          <ul>
+            <h3>{project.name}</h3>;
+            <img src={project.img} alt={project.alt} />
+            <p>{project.body}</p>
+          </ul>
+        );
+      })}
     </section>
   );
 };
