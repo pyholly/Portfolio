0b7f7ae900bb0b18266ea33eff0915585a387794
pyholly <pyhshum@gmail.com>
2023-09-06 11:29:25 +0100

    add responsiveness to navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,30 +2,74 @@ import { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 
 export const Nav = () => {
-  const [clicked, setClicked] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  function handleClick() {
+    if (open) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+  }
+
   return (
     <nav className="bg-purple-400/50 sticky top-0 py-2 w-full">
-      <div className="md:flex justify-between">
+      <div className="flex justify-between">
         <div className="px-10">
           <a href="#Home">Logo</a>
         </div>
-        <div className="md:hidden ">
-          <AiOutlineMenu />
+
+        <div className="md:hidden flex flex-col justify-end">
+          <button onClick={handleClick} className="px-5 h-6">
+            <AiOutlineMenu />
+          </button>
+
+          <ul className={open === true ? "justify-end inline-block" : "hidden"}>
+            <li key={"home"}>
+              <a href="#Home" className="px-5 hover:text-gray-400">
+                Home
+              </a>
+            </li>
+            <li key={"about"}>
+              <a href="#About" className="px-5 hover:text-gray-400">
+                About
+              </a>
+            </li>
+            <li key={"projects"}>
+              <a href="#Projects" className="px-5 hover:text-gray-400">
+                Projects
+              </a>
+            </li>
+            <li key={"contact"}>
+              <a href="#Contact" className="px-5 hover:text-gray-400">
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
-        <div>
-          <a href="#Home" className="px-5 hover:text-gray-400">
-            Home
-          </a>
-          <a href="#About" className="px-5 hover:text-gray-400">
-            About
-          </a>
-          <a href="#Projects" className="px-5 hover:text-gray-400">
-            Projects
-          </a>
-          <a href="#Contact" className="px-5 hover:text-gray-400">
-            Contact
-          </a>
-        </div>
+
+        <ul className="hidden md:inline-flex">
+          <li key={"home"}>
+            <a href="#Home" className="px-5 hover:text-gray-400">
+              Home
+            </a>
+          </li>
+          <li key={"about"}>
+            <a href="#About" className="px-5 hover:text-gray-400">
+              About
+            </a>
+          </li>
+          <li key={"projects"}>
+            <a href="#Projects" className="px-5 hover:text-gray-400">
+              Projects
+            </a>
+          </li>
+          <li key={"contact"}>
+            <a href="#Contact" className="px-5 hover:text-gray-400">
+              Contact
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
